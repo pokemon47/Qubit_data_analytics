dc542ab85127f2357c2c1b9fe5363f36a43c3f0f
Jack-Balcombe <j.balcombe@protonmail.com>
2025-03-20 13:17:30 +1100

    fix linting (part 2)

--- test.py
+++ test.py
@@ -3,23 +3,15 @@ from app import app  # Import the Flask app from app.py
 
 
 class TestFlaskAPI(unittest.TestCase):
-
-
-<< << << < HEAD
-
-    # Assuming your Flask API is running locally on port 5000
-    api_url = "http://127.0.0.1:5000/predict"
-== == == =
-
+    
     def setUp(self):
         self.client = app.test_client()
         self.client.testing = True
-
+    
     def test_server_is_active(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
         print(f"-{self._testMethodName} passed")
->>>>>> > 8ee6e30b71a907ebbe51a25c3a764d040f67fbbe
 
     def test_predict_positive_titles(self):
         data = {
@@ -31,22 +23,11 @@ class TestFlaskAPI(unittest.TestCase):
         }
         response = self.client.post("/predict", json=data)
         self.assertEqual(response.status_code, 200)
-<< << << < HEAD
-
-        result = response.json()
-        self.assertEqual(result["total"], 3)
-        self.assertGreater(result["total_positive"], result["total_negative"],
-                           "positive sentiments must be greater than negative sentiments")
-
-        # print("test_predict_positive_titles passed")
-== == == =
-
+        
         result = response.json
         self.assertEqual(result["total"], 3)
-        self.assertGreater(result["total_positive"], result["total_negative"],
-                           "positive sentiments must be greater than negative sentiments")
-
->>>>>> > 8ee6e30b71a907ebbe51a25c3a764d040f67fbbe
+        self.assertGreater(result["total_positive"], result["total_negative"], "positive sentiments must be greater than negative sentiments")
+        
         print(f"-{self._testMethodName} passed")
 
     def test_predict_mixed_titles(self):
@@ -62,20 +43,10 @@ class TestFlaskAPI(unittest.TestCase):
         }
         response = self.client.post("/predict", json=data)
         self.assertEqual(response.status_code, 200)
-<<<<<<< HEAD
-
-        result = response.json()
-        # self.assertEqual(result["total"], 3)
-        self.assertGreater(result["total_negative"], result["total_positive"],
-                           "negative sentiments must be greater than positive sentiments")
-
-        # print("test_predict_mixed_titles passed")
-=======
         
         result = response.json
         self.assertGreater(result["total_negative"], result["total_positive"], "negative sentiments must be greater than positive sentiments")
         
->>>>>>> 8ee6e30b71a907ebbe51a25c3a764d040f67fbbe
         print(f"-{self._testMethodName} passed")
 
     def test_empty_titles(self):
@@ -85,22 +56,12 @@ class TestFlaskAPI(unittest.TestCase):
         data = {"titles": []}
         response = self.client.post("/predict", json=data)
         self.assertEqual(response.status_code, 200)
-<<<<<<< HEAD
-
-        result = response.json()
-        self.assertEqual(result["total"], 0)
-        self.assertEqual(result["total_positive"], 0)
-        self.assertEqual(result["total_negative"], 0)
-
-        # print("test_empty_titles passed")
-=======
         
         result = response.json
         self.assertEqual(result["total"], 0)
         self.assertEqual(result["total_positive"], 0)
         self.assertEqual(result["total_negative"], 0)
         
->>>>>>> 8ee6e30b71a907ebbe51a25c3a764d040f67fbbe
         print(f"-{self._testMethodName} passed")
 
     def test_invalid_input(self):
@@ -110,18 +71,10 @@ class TestFlaskAPI(unittest.TestCase):
         data = {"titles": "Tesla stocks surged after good earnings report"}
         response = self.client.post("/predict", json=data)
         self.assertEqual(response.status_code, 400)
-<<<<<<< HEAD
-
-        result = response.json()
-        self.assertEqual(result["error"], "'titles' must be a list of strings")
-
-        # print("test_invalid_input passed")
-=======
         
         result = response.json
         self.assertEqual(result["error"], "'titles' must be a list of strings")
         
->>>>>>> 8ee6e30b71a907ebbe51a25c3a764d040f67fbbe
         print(f"-{self._testMethodName} passed")
 
 
